041a833413a5e9b9696a3fb335be75d5dce6b9ff
Marat Martypasha <maratmartypasha@gmail.com>
2025-03-06 09:58:26 +0700

    Add files via upload

---
+++ Soal[1b]_187231096_Week8-2.py
@@ -0,0 +1,8 @@
+print("masukkan suhu dalam reamur")
+reamur = float(input())
+celcius = reamur / 0.8
+fahrenheit = reamur * 2.25 + 32
+print("hasil konversi reamur ke celcius")
+print(celcius)
+print("hasil konversi reamur ke fahrenheit")
+print(fahrenheit)
