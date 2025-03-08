bd1f645e16f3a2d8585ace90dd0dc6114b213c62
Marat Martypasha <maratmartypasha@gmail.com>
2025-03-08 22:05:49 +0700

    Add files via upload

---
+++ Soal[1c]_187231096_Week8-2.py
@@ -0,0 +1,8 @@
+print("masukkan suhu dalam fahrenheit")
+fahrenheit = float(input())
+reamur = float(4) / 9 * (fahrenheit - 32)
+celcius = (fahrenheit - 32) * 5 / 9
+print("hasil konversi fahrenheit ke reamur")
+print(reamur)
+print("hasil konversi fahrenheit ke celcius")
+print(celcius)
